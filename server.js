297ffb161197af1dbfdd1cadb0fcf53a31f4574d
Vladimir Mikulin <36378514+vvvmvvv@users.noreply.github.com>
2020-05-06 02:22:14 +0300

    final v2 heroku

--- server.js
+++ server.js
@@ -21,6 +21,68 @@ app.use(express.json());
 app.use('/api', tasks);
 app.use(express.urlencoded({ extended: true }))
 
+const taskSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 255
+    },
+    isChecked: {
+        type: Boolean,
+        default: false
+    },
+    dates: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Task = mongoose.model('tasks', taskSchema );
+
+app.get('/api/tasks', async (req, res) => {
+    
+    try{
+        const tasks = await Task.find();
+        res.json(tasks);
+    }catch(err){
+        res.json({message: err});
+    }
+});
+
+app.post('/api/tasks', async (req, res) => {
+    const task = new Task({
+        title: req.body.title
+    });
+
+    try{
+        const savedTask = await task.save();
+        res.json(savedTask);
+    }catch(err) {
+        res.json({message: err});
+    }
+});
+
+app.delete("/api/task/:id", async (req, res) => {
+    try{
+        const removedTask = await Task.deleteOne({_id: req.params.id});
+        res.json(removedTask);
+    } catch(err){
+        res.json({message: err});
+    }
+    
+});
+
+app.put("/api/task/:id", async (req, res) => {
+    try{
+        const updatedTask = await Task.findByIdAndUpdate(req.params.id,req.body);
+        res.json(updatedTask);
+
+    }catch(err){
+        res.json({message: err});
+    }
+})
+
 app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/client/index.html'));
   });
