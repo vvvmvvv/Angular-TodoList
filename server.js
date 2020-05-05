59d963814aa0fec72740c2ce17329066fb2bed04
Vladimir Mikulin <36378514+vvvmvvv@users.noreply.github.com>
2020-05-05 23:48:13 +0300

    heroku final

--- server.js
+++ server.js
@@ -6,12 +6,7 @@ const cors = require('cors');
 const app = express();
 const path = require('path');
 
-// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/client'));
-
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/client/index.html'));
-  });
 
 dotenv.config();
 
@@ -25,6 +20,11 @@ app.use(express.json());
 
 app.use('/api', tasks);
 app.use(express.urlencoded({ extended: true }))
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/client/index.html'));
+  });
+
 
 app.listen(process.env.PORT || 8080, ()=>{
     console.log("Server start!")
