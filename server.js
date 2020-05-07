321e6c596c120e9a9cc1c260f5c34e5be15e1269
Vladimir Mikulin <36378514+vvvmvvv@users.noreply.github.com>
2020-05-07 13:02:16 +0300

    backend  update

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-//const cors = require('cors');
-const app = express();
 const path = require('path');
-const mongodb = require('mongodb');
+const tasks = require("./routes/tasks");
+
+const app = express();
 
 dotenv.config();
 
 app.use(express.static('./dist/angular-heroku'));
-//app.use(express.static(__dirname + '/dist'));
-
-var db;
-
-//app.use(cors());
 app.use(express.json());
-//app.use('/api', tasks);
 app.use(express.urlencoded({ extended: true }))
+
+app.use('/api', tasks);
 
 mongoose.connect(process.env.MONGODB_URI, { useUnifiedTopology: true , useNewUrlParser: true }, function (err) {
   if (err) {
@@ -31,67 +27,6 @@ mongoose.connect(process.env.MONGODB_URI, { useUnifiedTopology: true , useNewUrl
     console.log("App now running on port", port);
   });
 });
-
-const taskSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-        min: 6,
-        max: 255
-    },
-    isChecked: {
-        type: Boolean,
-        default: false
-    },
-    dates: {
-        type: Date,
-        default: Date.now
-    }
-});
-
-const Task = mongoose.model('tasks', taskSchema );
-
-app.get('/api/tasks', async (req, res) => {
-    try{
-        const tasks = await Task.find();
-        res.json(tasks);
-    }catch(err){
-        res.json({message: err});
-    }
-});
-
-app.post('/api/tasks', async (req, res) => {
-    const task = new Task({
-        title: req.body.title
-    });
-
-    try{
-        const savedTask = await task.save();
-        res.json(savedTask);
-    }catch(err) {
-        res.json({message: err});
-    }
-});
-
-app.delete("/api/task/:id", async (req, res) => {
-    try{
-        const removedTask = await Task.deleteOne({_id: req.params.id});
-        res.json(removedTask);
-    } catch(err){
-        res.json({message: err});
-    }
-    
-});
-
-app.put("/api/task/:id", async (req, res) => {
-    try{
-        const updatedTask = await Task.findByIdAndUpdate(req.params.id,req.body);
-        res.json(updatedTask);
-
-    }catch(err){
-        res.json({message: err});
-    }
-})
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '/dist/angular-heroku/index.html'));
